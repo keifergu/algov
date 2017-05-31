e883f92dec55c1ff43fb205e4786aaa2f3a8e47d
Keifer.Gu <keifergu@gmail.com>
2017-05-31 22:24:50 +0800

    修改 menu 的生成方式

--- src/components/AlgorithmList/index.js
+++ src/components/AlgorithmList/index.js
@@ -7,6 +7,31 @@ const { Sider, Content } = Layout;
 
 class AlgorithmList extends Component {
     render() {
+        const menuList = [
+            {
+                key: 'sort',
+                icon: 'user',
+                title: '排序',
+                items:[
+                    ['bubbleSort','冒泡排序'],
+                    ['inserSort','插入排序'],
+                    ['mergeSort','合并排序'],
+                    ['quickSort','快速排序']
+                ]
+            },
+            {
+                key: 'map',
+                icon: 'laptop',
+                title: '图算法',
+                items:[]
+            },
+            {
+                key: 'dynamic',
+                icon: 'notification',
+                title: '动态规划',
+                items:[]
+            },
+        ];
         return (
             <Layout>
                 <Sider width={200} style={{ background: '#fff' }}>
@@ -16,24 +41,11 @@ class AlgorithmList extends Component {
                     defaultOpenKeys={['sort']}
                     style={{ height: '100%' }}
                     >
-                    <SubMenu key="sort" title={<span><Icon type="user" />排序</span>}>
-                        <Menu.Item key="1">冒泡排序</Menu.Item>
-                        <Menu.Item key="2">插入排序</Menu.Item>
-                        <Menu.Item key="3">合并排序</Menu.Item>
-                        <Menu.Item key="4">快速排序</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="map" title={<span><Icon type="laptop" />图算法</span>}>
-                        <Menu.Item key="5">option5</Menu.Item>
-                        <Menu.Item key="6">option6</Menu.Item>
-                        <Menu.Item key="7">option7</Menu.Item>
-                        <Menu.Item key="8">option8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="dynamic" title={<span><Icon type="notification" />动态规划</span>}>
-                        <Menu.Item key="9">option9</Menu.Item>
-                        <Menu.Item key="10">option10</Menu.Item>
-                        <Menu.Item key="11">option11</Menu.Item>
-                        <Menu.Item key="12">option12</Menu.Item>
-                    </SubMenu>
+                    {menuList.map((menu) => (
+                        <SubMenu key={menu.key} title={<span><Icon type={menu.icon} />{menu.title}</span>}>
+                            {menu.items.map((item) => <Menu.Item key={item[0]}>{item[1]}</Menu.Item>)}
+                        </SubMenu>
+                    ))}
                     </Menu>
                 </Sider>
                 <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
